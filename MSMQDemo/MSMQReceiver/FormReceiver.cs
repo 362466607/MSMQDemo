1ca4d45c390554b4818e68cef8fdd274f8b90b5d
chenhuajin <chenhuajin@misrobot.com>
2020-01-13 19:07:10 +0800

    Add Project...

---
+++ MSMQDemo/MSMQReceiver/FormReceiver.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+
+namespace MSMQReceiver
+{
+    public partial class FormReceiver : Form
+    {
+        private MessageQueue queue;
+        private string path = ".\\Private$\\myQueue";
+
+        public FormReceiver()
+        {
+            InitializeComponent();
+            CreateMessageQueue();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RecvStringMessage();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RecvImageMessage();
+        }
+
+        private void CreateMessageQueue()
+        {
+            if (MessageQueue.Exists(path))
+            {
+                queue = new MessageQueue(path);
+            }
+            else
+            {
+                queue = MessageQueue.Create(path);
+            }
+        }
+
+        public void RecvStringMessage()
+        {
+            if (queue.GetAllMessages().Length == 0)
+            {
+                MessageBox.Show("消息队列为空");
+                return;
+            }
+            System.Messaging.Message message = queue.Receive();
+
+            message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+            MessageBox.Show(message.Body.ToString());
+        }
+
+        public void RecvImageMessage()
+        {
+            if (queue.GetAllMessages().Length == 0)
+            {
+                MessageBox.Show("消息队列为空");
+                return;
+            }
+            System.Messaging.Message message = queue.Receive();
+
+            message.Formatter = new BinaryMessageFormatter();
+
+            Image image = (Image)message.Body;
+
+            Form form = new Form();
+            form.Width = 1024;
+            form.Height = 768;
+            PictureBox pbox = new PictureBox();
+            pbox.Width = 1024;
+            pbox.Height = 768;
+            pbox.SizeMode = PictureBoxSizeMode.Zoom;
+            pbox.Image = image;
+            form.Controls.Add(pbox);
+            form.ShowDialog();
+        }
+    }
+}
---
+++ MSMQDemo/MSMQSender/FormSender.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+
+namespace MSMQSender
+{
+    public partial class FormSender : Form
+    {
+        private MessageQueue queue;
+        private string path = ".\\Private$\\myQueue";
+
+        public FormSender()
+        {
+            InitializeComponent();
+            CreateMessageQueue();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage(this.textBox1.Text);
+        }
+
+        private void CreateMessageQueue()
+        {
+            if (MessageQueue.Exists(path))
+            {
+                queue = new MessageQueue(path);
+            }
+            else
+            {
+                queue = MessageQueue.Create(path);
+            }
+        }
+
+        private void SendMessage(string str)
+        {
+            System.Messaging.Message message = new System.Messaging.Message();
+            message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+            message.Body = str;
+            queue.Send(message);
+            MessageBox.Show("消息发送成功");
+        }
+
+        private void SendMessage(Image image)
+        {
+            System.Messaging.Message message = new System.Messaging.Message();
+            message.Formatter = new BinaryMessageFormatter();
+            message.Body = image;
+            queue.Send(message);
+            MessageBox.Show("图像发送成功");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "图像文件|*.jpg;*.bmp;*.png";
+            open.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Bitmap.FromFile(open.FileName);
+                SendMessage(image);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(queue.GetAllMessages().Length.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            queue.Purge();
+        }
+    }
+}
